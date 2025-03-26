6492a999ba74170835bc20420e5de96045a3c2a9
Muhammad Asad luqman <124214469+MAsadLuqman@users.noreply.github.com>
2025-03-26 19:55:33 +0500

    set reply section

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -9,27 +9,26 @@ use Illuminate\Http\Request;
 class CommentController extends Controller
 {
 
-    public function index(Request $request){
-        $comments = Comment::with(['user'])->where('commentable_id', $request->postID)->orderby('created_at', 'desc')->paginate(4);
-        if($request->page > $comments->lastPage()){
-            $view = '';
-            return response()->json(['html'=>$view, 'isLoaded'=>true]);
-        }
-        if($request->ajax()){
-            $view = view('public.commentsshow',compact('comments'))->render();
-            return response()->json(['html'=>$view , 'isLoaded'=>false]);
-        }
-        return view('public.view-blogs',compact('comments'));
-    }
-
-    public function show(Request $request){
-        $comments = Comment::with(['user','replies.user'])->where('commentable_id', $request->postID)
-            ->whereNull('parent_id')
-            ->orderBy('created_at', 'desc')
-            ->paginate(10);
-
-        return view('public.show',compact('comments'));
-    }
+//    public function index(Request $request){
+//        $comments = Comment::with(['user'])->where('commentable_id', $request->postID)->orderby('created_at', 'desc')->paginate(4);
+//        if($request->page > $comments->lastPage()){
+//            $view = '';
+//            return response()->json(['html'=>$view, 'isLoaded'=>true]);
+//        }
+//        if($request->ajax()){
+//            $view = view('public.commentsshow',compact('comments'))->render();
+//            return response()->json(['html'=>$view , 'isLoaded'=>false]);
+//        }
+//        return view('public.view-blogs',compact('comments'));
+//    }
+//
+//    public function show(Request $request){
+//        $comments = Comment::with(['user','replies.user'])->where('commentable_id', $request->postID)
+//            ->whereNull('parent_id')
+//            ->orderBy('created_at', 'desc')
+//            ->paginate(10);
+//        return view('public.show',compact('comments'));
+//    }
 
     Public function store(Request $request){
         $data = $request->validate([
